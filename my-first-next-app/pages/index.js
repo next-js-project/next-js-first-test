fbde7c5b0bd715b17360140333f6aa96f7196e1a
dev-do <dev-do@naver.com>
2021-03-27 20:27:37 +0900

    getServerSideProps complete!

--- my-first-next-app/pages/index.js
+++ my-first-next-app/pages/index.js
@@ -1,22 +1,20 @@
-import fetch from "isomorphic-unfetch";
+import React, { useState } from "react";
+import Link from "next/link";
 
-const App = ({ user }) => {
-  const username = user && user.name;
-  return <div>{username}</div>;
-};
-
-export const getServerSideProps = async () => {
-  try {
-    const res = await fetch("https://api.github.com/users/jerrynim");
-    if (res.status === 200) {
-      const user = await res.json();
-      return { props: { user } };
-    }
-    return { props: {} };
-  } catch (e) {
-    console.log(e);
-    return { props: {} };
-  }
+const App = () => {
+  const [username, setUsername] = useState("");
+  return (
+    <div>
+      <label>
+        username
+        <input value={username} onChange={(e) => setUsername(e.target.value)} />
+      </label>
+      <p>{username} 깃허브 검색하기</p>
+      <Link href={`/users/${username}`}>
+        <a>검색하기</a>
+      </Link>
+    </div>
+  );
 };
 
 export default App;
