4610fca29ac83f76ee7c6db91b93e16bc6ecbc4f
dev-do <dev-do@naver.com>
2021-03-28 21:04:10 +0900

    getInitialProps complete! 42page!

--- my-first-next-app/pages/users/[name].js
+++ my-first-next-app/pages/users/[name].js
@@ -1,23 +1,23 @@
 import fetch from "isomorphic-unfetch";
 
 const Name = ({ user }) => {
+  console.log(user);
   const username = user && user.name;
   return <div>{username}</div>;
 };
 
-export const getServerSideProps = async ({ query }) => {
+Name.getInitialProps = async ({ query }) => {
   const { name } = query;
   try {
     const res = await fetch(`https://api.github.com/users/${name}`);
     if (res.status === 200) {
       const user = await res.json();
-      console.log(user);
-      return { props: { user } };
+      return { user };
     }
     return { props: {} };
   } catch (e) {
     console.log(e);
-    return { props: {} };
+    return {};
   }
 };
 
